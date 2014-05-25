05bbe556c6addab035f0329864a3c83acfef5959
Ambrosio Torres Galvis <atorresgalvis@gmail.com>
2014-05-25 01:59:45 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -66,3 +66,24 @@ mean_sd<-matrix(c(total_mean, total_sd), nrow=9, ncol=2, byrow=F) ; colnames(mea
 rownames(mean_sd) <- c("body_acc_x", "body_acc_y", "body_acc_z", "body_gyro_x", "body_gyro_y", "body_gyro_z", "total_acc_x", "total_acc_y", "total_acc_z")
 write.table(mean_sd, "total_means_sd.txt")
 mean_sd
+##obtain the means/average of each individual person to each variable.
+ind_mean <- NULL
+total_ind_mean <- NULL
+for (i in 1:9) {
+         s<-split(complete_data[[i]], complete_data[[i]][[1]])
+         for (j in 1:30) {
+                 idv <- s[[j]][[3]]
+                 for (k in 4:130) {
+                         new_idv<-s[[j]][[k]]
+                         idv <-c(idv, new_idv)     
+                 }
+                 ind_n_mean<-mean(idv)
+                 ind_mean <- c(ind_mean, ind_n_mean)
+         }
+         total_ind_mean <- rbind(total_ind_mean, ind_mean) 
+
+}
+rownames(total_ind_mean) <- c("body_acc_x", "body_acc_y", "body_acc_z", "body_gyro_x", "body_gyro_y", "body_gyro_z", "total_acc_x", "total_acc_y", "total_acc_z")
+colnames(total_ind_mean)<- paste(c("ind"), 1:30, sep="")
+write.table(total_ind_mean, "total_means_by_ind.txt")
+total_ind_mean
